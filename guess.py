8a91c267bb89150a545cb7eb172105dc5e1b2de0
Mike Davis <mikedavisrocha@gmail.com>
2020-06-05 09:54:31 -0300

    Adding while and if, elif, else to check the input

--- guess.py
+++ guess.py
@@ -9,3 +9,17 @@ print("Try to guess the number I'm thinking...")
 print("TIP: It's a number between 1 and 10...")
 correct_answer = generate_number()
 guesses = 1
+
+while guesses <= 10:
+    user_input = int(input())
+    if user_input < correct_answer and guesses != 10:
+        print("Your guess is too low. Try again.")
+    elif user_input > correct_answer and guesses != 10:
+        print("Your guess is too high. Try again.")
+    elif user_input == correct_answer:
+        print(f"YOU GOT IT RIGHT WITH {guesses} ATTEMPTS!!!")
+        break
+    else:
+        print(f"SORRY! THE GAME IS OVER. I WAS THINKING IN THE NUMBER {correct_answer}")            
+    guesses += 1
+    
