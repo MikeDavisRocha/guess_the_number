714ed6b3ac62884bfc1da41ed390a173a41ff4c9
Mike Davis <mikedavisrocha@gmail.com>
2020-06-05 09:51:37 -0300

    Adding the initial info and variables to correct answer and guesses

--- guess.py
+++ guess.py
@@ -4,3 +4,8 @@ def generate_number():
     return randint(1, 10)
 
 
+print("----- Guess the Number -----")
+print("Try to guess the number I'm thinking...")
+print("TIP: It's a number between 1 and 10...")
+correct_answer = generate_number()
+guesses = 1
